1c621e04e873934401779008382594159bdf2e0e
synerr0 <tilakadroja@gmail.com>
2024-03-18 00:42:06 +0530

    added new projects

--- app/components/other/PreLoader.tsx
+++ app/components/other/PreLoader.tsx
@@ -94,9 +94,9 @@ const PreLoader: React.FC = () => {
                     height: "60px",
                 }}
             >
-                <span>Developer</span>
-                <span> / </span>
-                <span>Designer</span>
+                <span>MERN Developer</span>
+                {/* <span> / </span>
+                <span>Designer</span> */}
                 <div className="sub hidden"></div>
             </div>
         </div>
