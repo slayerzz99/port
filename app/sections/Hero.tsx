7f950d414c37216ef6e81a4c69b0f61efe6c5b43
tilak <141637800+tilak-git@users.noreply.github.com>
2024-12-17 22:44:28 +0530

    Update Hero.tsx

--- app/sections/Hero.tsx
+++ app/sections/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
                 <div
                     className={`relative flex flex-col items-center justify-center ${spaceGrotesk.className} pointer-events-none`}
                 >
-                    {/* <Logo width={100} height={100} /> */}
+                     <Logo width={100} height={100} />
                 </div>
             </div>
         </motion.section>
